7eb47a3c7f5fae0aaf9ec3eb0fd07b5d09f991be
WilliamFoley <CaptainTrips1500@proton.me>
2025-01-09 09:40:02 -0500

    removed unneccessary files, added folders, updated index and app files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./nav/Nav.js";
+import "./about/About";
+import Projects from "./projects/Projects";
+import Contact from "./contact/Contact";
+import "./styles/app.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Route path="/" element={<About />}/>
+      <Route path="/projects" element={<Projects />}/>
+      <Route path="/contact" elemet={<Contact />}/>
+    </Router>
   );
 }
 
